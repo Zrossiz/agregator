d8fe59806caf3c8289f30970ce1180980538196d
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-17 18:39:53 +0300

    upd: get menu from getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,45 @@
-import Head from "next/head";
 import { Noto_Sans } from "next/font/google";
 import { withLayout } from "@/layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "@/interfaces/menu.interface";
 
 const inter = Noto_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
 });
 
-function Home() {
+function Home({ menu }: HomeProps) {
   return (
     <>
-      <main className={inter.className}>Контент</main>
+      <main className={inter.className}>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </main>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
