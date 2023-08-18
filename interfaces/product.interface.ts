65dd4187ebfee29bad562b155ad4e3d16088dadf
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-18 15:47:50 +0300

    upd: added getStaticPaths query

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,45 @@
+export interface ProductModel {
+  _id: string;
+  categories: string[];
+  tags: string[];
+  title: string;
+  image: string;
+  description: string;
+  link: string;
+  price: number;
+  credit: number;
+  oldPrice: number;
+  characteristics: ProductCharacteristic[];
+  advantages?: string;
+  initialRating: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  html: string;
+  blog: Blog;
+  companyId: string;
+  clicks: number;
+  reviews: ReviewModel[];
+  reviewCount: number;
+  reviewAvg?: number;
+}
+
+export interface ReviewModel {
+  _id: string;
+  name: string;
+  title: string;
+  description: string;
+  rating: number;
+  createdAt: Date;
+}
+
+export interface Blog {
+  text: string;
+  _id: string;
+  bigImage?: string;
+}
+
+export interface ProductCharacteristic {
+  name: string;
+  value: string;
+}
