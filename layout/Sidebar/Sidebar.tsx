4fbbcb6bc9e60edfdc4e2199a5d7bb8f64afd7d4
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-21 18:39:57 +0300

    upd: added logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { SidebarProps } from "./Sidebar.props";
 import styles from "./Sidebar.module.css";
 import Menu from "@/layout/Menu/Menu";
+import Logo from "../Logo.svg";
+import cn from "classnames";
 
-const Sidebar = ({ ...props }: SidebarProps) => {
+const Sidebar = ({ className, ...props }: SidebarProps) => {
   return (
-    <div {...props}>
+    <div {...props} className={cn(className, styles.sidebar)}>
+      <Logo className={styles.logo} />
+      <div>Поиск</div>
       <Menu />
     </div>
   );
