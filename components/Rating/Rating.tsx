06a4723a29d5b66c901e75a32ec58339d4f68c40
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-27 12:56:03 +0300

    fix: added ref for inputs

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,73 +1,79 @@
-import { useState, useEffect, KeyboardEvent } from "react";
+import {
+  useState,
+  useEffect,
+  KeyboardEvent,
+  forwardRef,
+  ForwardedRef,
+} from "react";
 import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import StarIcon from "./Star.svg";
 
-export const Rating = ({
-  isEditable = false,
-  rating,
-  children,
-  setRating,
-  ...props
-}: RatingProps) => {
-  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
-    new Array(5).fill(<></>)
-  );
+// eslint-disable-next-line react/display-name
+export const Rating = forwardRef(
+  (
+    { isEditable = false, rating, children, setRating, ...props }: RatingProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
+      new Array(5).fill(<></>)
+    );
 
-  useEffect(() => {
-    constructRating(rating);
-  }, [rating]);
+    useEffect(() => {
+      constructRating(rating);
+    }, [rating]);
 
-  const constructRating = (currentRating: number) => {
-    const updatedArray = ratingArray.map((r: JSX.Element, index: number) => {
-      return (
-        <span key={index}>
-          <StarIcon
-            className={cn(styles.star, {
-              [styles.filled]: index < currentRating,
-              [styles.editable]: isEditable,
-            })}
-            onMouseEnter={() => changeDisplay(index + 1)}
-            onMouseLeave={() => changeDisplay(rating)}
-            onClick={() => onClick(index + 1)}
-            tabIndex={isEditable ? 0 : -1}
-            onKeyDown={(event: KeyboardEvent<SVGElement>) =>
-              isEditable && handleSpace(index + 1, event)
-            }
-          />
-        </span>
-      );
-    });
-    setRatingArray(updatedArray);
-  };
+    const constructRating = (currentRating: number) => {
+      const updatedArray = ratingArray.map((r: JSX.Element, index: number) => {
+        return (
+          <span key={index}>
+            <StarIcon
+              className={cn(styles.star, {
+                [styles.filled]: index < currentRating,
+                [styles.editable]: isEditable,
+              })}
+              onMouseEnter={() => changeDisplay(index + 1)}
+              onMouseLeave={() => changeDisplay(rating)}
+              onClick={() => onClick(index + 1)}
+              tabIndex={isEditable ? 0 : -1}
+              onKeyDown={(event: KeyboardEvent<SVGElement>) =>
+                isEditable && handleSpace(index + 1, event)
+              }
+            />
+          </span>
+        );
+      });
+      setRatingArray(updatedArray);
+    };
 
-  const handleSpace = (index: number, event: KeyboardEvent<SVGElement>) => {
-    if (event.code != "Space" || !setRating) {
-      return;
-    }
-    setRating(index);
-  };
+    const handleSpace = (index: number, event: KeyboardEvent<SVGElement>) => {
+      if (event.code != "Space" || !setRating) {
+        return;
+      }
+      setRating(index);
+    };
 
-  const onClick = (index: number) => {
-    if (!isEditable || !setRating) {
-      return;
-    }
-    setRating(index);
-  };
+    const onClick = (index: number) => {
+      if (!isEditable || !setRating) {
+        return;
+      }
+      setRating(index);
+    };
 
-  const changeDisplay = (index: number) => {
-    if (!isEditable) {
-      return;
-    }
-    constructRating(index);
-  };
+    const changeDisplay = (index: number) => {
+      if (!isEditable) {
+        return;
+      }
+      constructRating(index);
+    };
 
-  return (
-    <div {...props}>
-      {ratingArray.map((rating, index) => {
-        return <span key={index}>{rating}</span>;
-      })}
-    </div>
-  );
-};
+    return (
+      <div ref={ref} {...props}>
+        {ratingArray.map((rating, index) => {
+          return <span key={index}>{rating}</span>;
+        })}
+      </div>
+    );
+  }
+);
--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -1,7 +1,20 @@
 import styles from "./Textarea.module.css";
 import cn from "classnames";
 import { TextareaProps } from "./Textarea.props";
+import { ForwardedRef, forwardRef } from "react";
 
-export const Textarea = ({ className, ...props }: TextareaProps) => {
-  return <textarea className={cn(className, styles.textarea)} {...props} />;
-};
+// eslint-disable-next-line react/display-name
+export const Textarea = forwardRef(
+  (
+    { className, ...props }: TextareaProps,
+    ref: ForwardedRef<HTMLTextAreaElement>
+  ) => {
+    return (
+      <textarea
+        className={cn(className, styles.textarea)}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
