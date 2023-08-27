c9b23bdb377b6ea123ade6782eeabe8452995c39
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-27 12:42:47 +0300

    upd: added react-hook-form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,4 +1,5 @@
 import { ReviewFormProps } from "./ReviewForm.props";
+import { useForm, Controller } from "react-hook-form";
 import styles from "./ReviewForm.module.css";
 import CloseIcon from "./close.svg";
 import cn from "classnames";
@@ -6,22 +7,47 @@ import { Input } from "../Input/Input";
 import { Rating } from "../Rating/Rating";
 import { Textarea } from "../Textarea/Textarea";
 import { Button } from "../Button/Button";
+import { IReviewForm } from "./ReviewForm.interface";
 
 export const ReviewForm = ({
   productId,
   className,
   ...props
 }: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input placeholder="Имя" />
-        <Input placeholder="Заголовок отзыва" className={styles.title} />
+        <Input {...register("name")} placeholder="Имя" />
+        <Input
+          {...register("title")}
+          placeholder="Заголовок отзыва"
+          className={styles.title}
+        />
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0} />
+          <Controller
+            control={control}
+            name="rating"
+            render={({ field }) => (
+              <Rating
+                isEditable
+                rating={field.value}
+                setRating={field.onChange}
+              />
+            )}
+          />
         </div>
-        <Textarea placeholder="Текст отзыва" className={styles.description} />
+        <Textarea
+          placeholder="Текст отзыва"
+          className={styles.description}
+          {...register("description")}
+        />
         <div className={styles.submit}>
           <Button appearance="primary">Отправить</Button>
           <span className={styles.info}>
@@ -35,6 +61,6 @@ export const ReviewForm = ({
         <div>Спасибо, ваш отзыв будет опубликован после проверки.</div>
         <CloseIcon className={styles.close} />
       </div>
-    </>
+    </form>
   );
 };
