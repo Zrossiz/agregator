2b9becdd8afb49d19008c77a637ef219dd0dc519
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-28 21:28:16 +0300

    upd: added review animation

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,23 +2,26 @@ import React, { forwardRef, ForwardedRef } from "react";
 import { CardProps } from "./Card.props";
 import cn from "classnames";
 import styles from "./Card.module.css";
+import { motion } from "framer-motion";
 
-// eslint-disable-next-line react/display-name
-export const Card = forwardRef(
-  (
-    { color = "white", className, children, ...props }: CardProps,
-    ref: ForwardedRef<HTMLDivElement>
-  ) => {
-    return (
-      <div
-        className={cn(styles.card, className, {
-          [styles.blue]: color == "blue",
-        })}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </div>
-    );
-  }
+export const Card = motion(
+  // eslint-disable-next-line react/display-name
+  forwardRef(
+    (
+      { color = "white", className, children, ...props }: CardProps,
+      ref: ForwardedRef<HTMLDivElement>
+    ) => {
+      return (
+        <div
+          className={cn(styles.card, className, {
+            [styles.blue]: color == "blue",
+          })}
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </div>
+      );
+    }
+  )
 );
