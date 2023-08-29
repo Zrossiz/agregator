0647ab23ce5c61caa04abd1e6219cdbdf12b9742
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-29 14:56:34 +0300

    upd: added 404 500

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -13,27 +13,26 @@ import { firstLevelMenu } from "@/helpers/helpers";
 import { API } from "@/helpers/api";
 
 import TopPageComponent from "@/page-components/TopPageComponent/TopPageComponent";
+import { Error404 } from "../404";
 
 function TopPage({ firstCategory, page, products }: TopPageProps) {
+  if (!page || products) {
+    return <Error404 />;
+  }
   return (
     <>
-      {page && (
-        <>
-          {" "}
-          <Head>
-            <title>{page.metaTitle}</title>
-            <meta name="description" content={page.metaDescription} />
-            <meta property="og:title" content={page.title} />
-            <meta property="og:description" content={page.metaDescription} />
-            <meta property="og:type" content="article" />
-          </Head>
-          <TopPageComponent
-            firstCategory={firstCategory}
-            page={page}
-            products={products}
-          />
-        </>
-      )}
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name="description" content={page.metaDescription} />
+        <meta property="og:title" content={page.title} />
+        <meta property="og:description" content={page.metaDescription} />
+        <meta property="og:type" content="article" />
+      </Head>
+      <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+      />
     </>
   );
 }
@@ -52,7 +51,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
